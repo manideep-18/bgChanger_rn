48f23a71b5c8fb903183ac597416c75083dbbfff
manideep-18 <r8manideep@gmail.com>
2020-12-06 12:29:39 +0530

    styles configuration added

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text,View,StyleSheet,TouchableOpacity} from 'react-native'
 
 const App=()=>{
   return(
     <>
-    <Text>Manideep Ramapuram</Text>
+    <View style={[styles.container,{backgroundColor:"rgb(32,0,126)"}]}>
+      <Text>Manideep Ramapuram</Text>
+    </View>
     </>
   )
 }
 
 export default App
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
