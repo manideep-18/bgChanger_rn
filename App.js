585808c20b273d2820203a6cdb17c7b76653a61b
manideep-18 <r8manideep@gmail.com>
2020-12-06 12:52:48 +0530

    text styles added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const App=()=>{
   return(
     <>
     <View style={[styles.container,{backgroundColor:"rgb(32,0,126)"}]}>
-      <Text>Manideep Ramapuram</Text>
+      <Text style={styles.text}>Tap Me</Text>
     </View>
     </>
   )
@@ -18,5 +18,14 @@ const styles=StyleSheet.create({
     flex:1,
     justifyContent:'center',
     alignItems:'center'
+  },
+  text:{
+    fontSize:30,
+    backgroundColor:"#BB2CD9",
+    paddingVertical:10,
+    paddingHorizontal:40,
+    color:"#FFFFFF",
+    borderRadius:15,
+    textTransform:"uppercase"
   }
 })
